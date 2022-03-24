a94f79166065f74b2d91b6db9c9827ae627281f4
HiSeulgi <sugabida@gmail.com>
2022-03-24 11:33:38 +0700

    menambah job2 routing

---
+++ resources/views/biodata2.blade.php
@@ -0,0 +1,3 @@
+<?php
+    echo 'Nama : Sugab <br> Nim : 4.33.20.0.17 <br> Alamat : Baskoro <br> Hobi : Wibu';
+?>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,68 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat belajar Laravel cantik, semangatttt!! ♥';
+});
+
+Route::get('/biodata', function(){
+    return 'Nama : Sugab <br> Nim : 4.33.20.0.17 <br> Alamat : Baskoro <br> Hobi : Wibu';
+});
+
+Route::get('/mahasiswa/{prodi}', function($prodi){
+    return 'Mahasiswa prodi : '.$prodi;
+});
+
+Route::get('/mahasiswa2/{prodi?}', function($prodi=null){
+    if ($prodi == null) return 'Data program studi kosong';
+    return 'Mahasiswa prodi : '.$prodi;
+});
+
+Route::get('/mahasiswa3/{prodi?}', function($prodi='Teknologi Rekayasa Komputer'){
+    return 'Mahasiswa prodi : '.$prodi;
+});
+
+Route::get('/biodata2', function(){
+    return view('biodata2');
+});
+
+Route::group([], function(){
+    Route::get('/pertama', function(){
+        echo 'route pertama';
+    });
+    Route::get('/kedua', function(){
+        echo 'route kedua';
+    });
+    Route::get('/ketiga', function(){
+        echo 'route ketiga';
+    });
+});
+
+Route::group(['prefix' => 'latihan'], function(){
+    Route::get('/satu', function(){
+        echo 'Latihan 1';
+    });
+    Route::get('/dua', function(){
+        echo 'Latihan 2';
+    });
+    Route::get('/tiga', function(){
+        echo 'Latihan 3';
+    });
+});
+
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('/', function(){
+        return 'Halaman Home Admin';
+    });
+    Route::get('/posts', function(){
+        return 'Halaman Input Data Buku';
+    });
+    Route::get('/posts/simpan', function(){
+        return 'Data berhasil disimpan';
+    });
+});
+
+Route::name('kuliah')->group(function(){
+    Route::get('Teknologi Rekayasa Komputer', function(){
+        return 'Kuliah di Program Studi Teknologi Rekayasa Komputer';
+    });
 });
